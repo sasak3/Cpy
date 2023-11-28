08063036ba068d644f84a4d062db38bde2ca5dd5
heruahmad1 <138838307+heruahmad1@users.noreply.github.com>
2023-11-28 09:27:24 +0700

    Add files via upload

---
+++ install-bot.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+red='\e[1;31m'
+green='\e[0;32m'
+yell='\e[1;33m'
+NC='\e[0m'
+echo "OTW install bot Vps"
+echo "waiting......."
+echo "Progress..."
+sleep 3
+#install bot TELEGRAM
+cd /usr/bin 
+wget -O bot "https://raw.githubusercontent.com/heruahmad1/v4/main/bot.sh" && chmod +x /usr/bin/bot
+
+cd
+
+sleep 3
+clear
+read -n 1 -s -r -p "Press any key to back on menu"
+
+menu
