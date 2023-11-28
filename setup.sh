1d3a41bec93fe4213ace580e407db5e9f12d19fa
heruahmad1 <138838307+heruahmad1@users.noreply.github.com>
2023-11-28 18:56:37 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -194,12 +194,13 @@ wget https://raw.githubusercontent.com/heruahmad1/v4/main/Sshws/insshws.sh && ch
 clear
 #pasang rc clone ssh ovpn 
 ### Pasang Rclone
-function pasang_backup() {
-    judge "Memasang backup server"
-    wget https://raw.githubusercontent.com/artanodrop/v4/main/backup/set-br.sh &&  chmod +x set-br.sh && ./set-br.sh >/dev/null 2>&1
-    print_success "backup server"
-}
-
+echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+echo -e "$green          Install Auto Backup              $NC"
+echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+sleep 2
+clear
+wget https://raw.githubusercontent.com/heruahmad1/v4/main/backup/set-br.sh &&  chmod +x set-br.sh && ./set-br.sh >/dev/null 2>&1
+clear   
 #Instal slowdns
 ### Pasang SlowDNS
 echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
