666d419e3da1f2779b8da5bc616283605d6130ca
heruahmad1 <138838307+heruahmad1@users.noreply.github.com>
2023-11-30 20:58:47 +0800

    Update menu-vless.sh

--- menu/menu-vless.sh
+++ menu/menu-vless.sh
@@ -52,21 +52,6 @@ PERMISSION () {
 red='\e[1;31m'
 green='\e[1;32m'
 NC='\e[0m'
-# background validation
-
-        green_background="\033[42;37m"
-
-        red_background="\033[41;37m"
-
-        # color text 
-
-        yelow="\e[1;33m"
-
-        NC="\033[0m"
-
-        cyan="\e[1;36m"
-
-        white="\e[1;37m"
 green() { echo -e "\\033[32;1m${*}\\033[0m"; }
 red() { echo -e "\\033[31;1m${*}\\033[0m"; }
 PERMISSION
