90812433885873d820723941c0a32a21e92b69d4
heruahmad1 <138838307+heruahmad1@users.noreply.github.com>
2023-12-01 16:16:28 +0800

    Update menu-backup.sh

--- backup/menu-backup.sh
+++ backup/menu-backup.sh
@@ -1,84 +1,83 @@
 clear
 red='\033[0;31m'
-green='\033[1;32m'
+green='\033[0;32m'
 yellow='\033[0;33m'
 plain='\033[0m'
 blue='\033[0;34m'
 ungu='\033[1;35m'
-Green="\033[32m"
+Green="\033[1;32m"
 Red="\033[31m"
 WhiteB="\e[5;37m"
 BlueCyan="\e[5;36m"
 Green_background="\033[42;37m"
 Red_background="\033[41;37m"
+cyan="\e[1;36m"
 Suffix="\033[0m"
+green_background="\033[42;37m"
+red_background="\033[41;37m"
 
-
-echo -e "${ungu}++++++++++++++++++++++++++++++++++++++++++++"
-echo -e "${BlueCyan}              Menu Backup                   "
-echo -e "${ungu}++++++++++++++++++++++++++++++++++++++++++++"
+echo -e "${ungu} ┌───────────────────────────────┐${NC}"
+echo -e "${ungu} │${blue} .::::.  MENU BACKUP   .::::.  ${NC}"
+echo -e "${ungu} └───────────────────────────────┘${NC}"
 echo
+echo -e "${yellow} ┌───────────────────────────────┐${NC}"
+echo "      [1]. Backup"
+echo "      [2]. Restore"
+echo "      [3]. strt"
+echo "      [4]. limit speed"
+echo "      [5]. auto backup"
+echo -e "${yellow} └───────────────────────────────┘${NC}"
 echo
-echo -e "${BlueCyan}Pilih Nomor: "
-echo
-echo "1).Backup"
-echo "2).Restore"
-echo "3).strt"
-echo "4).limit speed"
-echo "5).autobackup"
-echo
-echo -e "${ungu}++++++++++++++++++++++++++++++++++++++++++++"
-echo
-echo -e "${green}"
-read -p    "Pilih Nomor └╼>>> " bro
+echo -e "
+read -p "  Select From Options [1-5] : " opt
 
 if [ $bro = 1 ] || [ $bro = 1 ]
 then
-figlet "backup" | lolcat
+figlet "backup"
 backup
-echo "--------------------------------------------------------"
+echo "${yellow} ┌───────────────────────────────────────────────┐"
 echo
-echo "Terimakasih sudah menggunakan layanan Premium SKY-TUNNEL"
+echo "${blue}   Terimakasih sudah menggunakan layanan Premium SKY-TUNNEL"
 echo
-echo "--------------------------------------------------------"
+echo "${yellow} └───────────────────────────────────────────────┘"
 fi
 
 if [ $bro = 2 ] || [ $bro = 2 ]
 then
 figlet "Restore" | lolcat
 restore
-echo "--------------------------------------------------------"
+echo "${yellow} ┌───────────────────────────────────────────────┐"
 echo
-echo "Terimakasih sudah menggunakan layanan Premium SKY-TUNNEL"
+echo "${blue}   Terimakasih sudah menggunakan layanan Premium SKY-TUNNEL"
 echo
-echo "--------------------------------------------------------"
+echo "${yellow} └───────────────────────────────────────────────┘"
 fi
 if [ $bro = 3 ] || [ $bro = 3 ]
 then
 figlet "strt"
 strt
-echo "--------------------------------------------------------"
+echo "${yellow} ┌───────────────────────────────────────────────┐"
 echo
-echo "Terimakasih sudah menggunakan layanan Premium SKY-TUNNEL"
+echo "${blue}   Terimakasih sudah menggunakan layanan Premium SKY-TUNNEL"
 echo
-echo "--------------------------------------------------------"
+echo "${yellow} └───────────────────────────────────────────────┘"
 fi
 if [ $bro = 4 ] || [ $bro = 4 ]
 then
 limitspeed
-echo "--------------------------------------------------------"
+echo "${yellow} ┌───────────────────────────────────────────────┐"
 echo
-echo "Terimakasih sudah menggunakan layanan Premium SKY-TUNNEL"
+echo "${blue}   Terimakasih sudah menggunakan layanan Premium SKY-TUNNEL"
 echo
-echo "--------------------------------------------------------"
+echo "${yellow} └───────────────────────────────────────────────┘"
 fi
 
 if [ $bro = 5 ] || [ $bro = 5 ]
 then
 autobackup
-echo "--------------------------------------------------------"
+echo "${yellow} ┌───────────────────────────────────────────────┐"
 echo
-echo "Terimakasih sudah menggunakan layanan Premium SKY-TUNNEL"
+echo "${blue}   Terimakasih sudah menggunakan layanan Premium SKY-TUNNEL"
 echo
-echo "--------------------------------------------------------"
+echo "${yellow} └───────────────────────────────────────────────┘"
 fi
