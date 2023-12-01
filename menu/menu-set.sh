b593dbc85920bef9ee2922f6831e7d3368710496
heruahmad1 <138838307+heruahmad1@users.noreply.github.com>
2023-12-02 02:24:36 +0800

    Update menu-set.sh

--- menu/menu-set.sh
+++ menu/menu-set.sh
@@ -19,18 +19,17 @@ echo -e "\e[33m │ ${cyan}[•4]${NC} Auto Backup data server\033[0m"
 echo -e "\e[33m │ ${cyan}[•5]${NC} Backup data server\033[0m"
 echo -e "\e[33m │ ${cyan}[•6]${NC} Restore data server\033[0m"
 echo -e "\e[33m │ ${cyan}[•7]${NC} Chnge Domain server\033[0m"
-echo -e "\e[33m │ ${cyan}[•8]${NC} Chnge port server\033[0m"
-echo -e "\e[33m │ ${cyan}[•9]${NC} Chnge banner server\033[0m"
-echo -e "\e[33m │ ${cyan}[10]${NC} Restart all service\033[0m"
-echo -e "\e[33m │ ${cyan}[11]${NC} Bandwith information\033[0m"
-echo -e "\e[33m │ ${cyan}[12]${NC} Chnge auto reboot\033[0m"
-echo -e "\e[33m │ ${cyan}[13]${NC} Speedtest server\033[0m"
-echo -e "\e[33m │ ${cyan}[14]${NC} Clear Log all service\033[0m"
-echo -e "\e[33m │ ${cyan}[15]${NC} Reboot server\033[0m"
+echo -e "\e[33m │ ${cyan}[•8]${NC} Chnge banner server\033[0m"
+echo -e "\e[33m │ ${cyan}[•9]${NC} Restart all service\033[0m"
+echo -e "\e[33m │ ${cyan}[10]${NC} Bandwith information\033[0m"
+echo -e "\e[33m │ ${cyan}[11]${NC} Chnge auto reboot\033[0m"
+echo -e "\e[33m │ ${cyan}[12]${NC} Speedtest server\033[0m"
+echo -e "\e[33m │ ${cyan}[13]${NC} Clear Log all service\033[0m"
+echo -e "\e[33m │ ${cyan}[14]${NC} Reboot server\033[0m"
 echo -e "\e[33m │ ${cyan}[•0]${NC} Back To Menu\033[0m"
 echo -e "\e[33m └──────────────────────────────────┘\033[0m"
 echo -e ""
-read -p " Select From Options [ 1 - 15 ] : "  opt
+read -p " Select From Options [ 1 - 14 ] : "  opt
 echo -e ""
 case $opt in
 1) clear ; rm -rf /root/updatefile.sh && wget https://raw.githubusercontent.com/heruahmad1/v4/main/updatefile.sh && chmod +x updatefile.sh && ./updatefile.sh ;;
@@ -40,14 +39,13 @@ case $opt in
 5) clear ; backup ; exit ;;
 6) clear ; restore ; exit ;;
 7) clear ; menu-domain ; exit ;;
-8) clear ; port-change ; exit ;;
-9) clear ; nano /etc/issue.net ; exit ;;  #ssh-vpn banner.conf
-10) clear ; restarts ; exit ;;
-11) clear ; bw ; exit ;;
-12) clear ; auto-reboot ; exit ;;
-13) clear ; speedtest ; exit ;;
-14) clear ; clearcache ; exit ;;
-15) clear ; reboot ; exit ;;
+8) clear ; nano /etc/issue.net ; exit ;;  #ssh-vpn banner.conf
+9) clear ; restarts ; exit ;;
+10) clear ; bw ; exit ;;
+11) clear ; auto-reboot ; exit ;;
+12) clear ; speedtest ; exit ;;
+13) clear ; clearcache ; exit ;;
+14) clear ; reboot ; exit ;;
 0) clear ; menu ; exit ;;
 x) exit ;;
 *) echo -e "" ; echo "salah tekan" ; sleep 1 ; menu-set ;;
