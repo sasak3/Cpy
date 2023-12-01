ac97b2be3720be3faf10b9e90d7f4e4c76a6407f
heruahmad1 <138838307+heruahmad1@users.noreply.github.com>
2023-12-02 02:16:45 +0800

    Update clearcache.sh

--- menu/clearcache.sh
+++ menu/clearcache.sh
@@ -3,10 +3,10 @@ MYIP=$(wget -qO- ipinfo.io/ip);
 clear
 echo ""
 echo ""
-echo -e "[ \033[32mInfo\033[0m ] Clear RAM Cache"
+echo -e "[ \033[32mInfo\033[0m ] Clear Log all service"
 echo 1 > /proc/sys/vm/drop_caches
 sleep 3
-echo -e "[ \033[32mok\033[0m ] Cache cleared"
+echo -e "[ \033[32mok\033[0m ] Log service cleared finish "
 echo ""
 echo "Back to menu in "
 menu
