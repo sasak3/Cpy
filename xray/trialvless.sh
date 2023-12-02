f65dc56442a3da24f039a52023365ae5702d8bef
heruahmad1 <138838307+heruahmad1@users.noreply.github.com>
2023-12-03 00:12:38 +0800

    Update trialvless.sh

--- xray/trialvless.sh
+++ xray/trialvless.sh
@@ -26,56 +26,57 @@ vless2="$(echo $vlesslink2 | base64 -w 0)"
 vless3="$(echo $vlesslink3 | base64 -w 0)"
 
 TEXT="
-<code>◇━━━━━━━━━━━━━━━━━◇</code>
-<code>  Premium Vless Account</code>
-<code>◇━━━━━━━━━━━━━━━━━◇</code>
-<code>Remarks      : </code> <code>${user}</code>
-<code>Domain       : </code> <code>${domain}</code>
-<code>Port TLS     : 443</code>
-<code>Port NTLS    : 80, 8080</code>
-<code>Port GRPC    : 443</code>
-<code>User ID      : </code> <code>${uuid}</code>
-<code>AlterId      : 0</code>
-<code>Security     : auto</code>
-<code>Network      : WS or gRPC</code>
-<code>Path vless   : </code> <code>/vless</code>
-<code>ServiceName  : </code> <code>/vless-grpc</code>
-<code>Expired On : </code> <code>$timer Minutes</code>
-<code>◇━━━━━━━━━━━━━━━━━◇</code>
-<code>Link TLS     :</code> 
+<code>━━━━━━━━━━━━━━━━━━━</code>
+<code>🔰 TRIAL XRAY/VLESS 🔰   </code>
+<code>━━━━━━━━━━━━━━━━━━━</code>
+<code>➣Remarks    : </code> <code>${user}</code>
+<code>➣Domain     : </code> <code>${domain}</code>
+<code>➣Port TLS   : </code> <code>443</code>
+<code>➣Port NTLS  : </code> <code>80, 8080</code>
+<code>➣Port GRPC  : </code> <code>443</code>
+<code>➣User ID    : </code> <code>${uuid}</code>
+<code>➣AlterId    : 0</code>
+<code>➣Security   : auto</code>
+<code>➣Network    : WS or gRPC</code>
+<code>➣Path       : </code> <code>/vless</code>
+<code>➣ServiceName: </code> <code>vless-grpc</code>
+<code>━━━━━━━━━━━━━━━━━━━</code>
+<code>➣Link TLS   :</code> 
 <code>${vless1}</code>
-<code>◇━━━━━━━━━━━━━━━━━◇</code>
-<code>Link NTLS    :</code> 
+<code>━━━━━━━━━━━━━━━━━━━</code>
+<code>➣Link NTLS  :</code> 
 <code>${vless2}</code>
-<code>◇━━━━━━━━━━━━━━━━━◇</code>
-<code>Link GRPC    :</code> 
+<code>━━━━━━━━━━━━━━━━━━━</code>
+<code>➣Link GRPC  :</code> 
 <code>${vless3}</code>
-<code>◇━━━━━━━━━━━━━━━━━◇</code>
+<code>━━━━━━━━━━━━━━━━━━━</code>
+<code>📅Expired On : $exp</code>
+🌏 @HRstores
 "
 
 curl -s --max-time $TIMES -d "chat_id=$CHATID&disable_web_page_preview=1&text=$TEXT&parse_mode=html" $URL >/dev/null
 
-echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-echo -e "\E[40;1;37m        Trial Xray/Vless        \E[0m"
-echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-echo -e "Remarks        : ${user}"
-echo -e "Domain         : ${domain}"
-echo -e "port TLS       : $tls"
-echo -e "port none TLS  : $none"
-echo -e "id             : ${uuid}"
-echo -e "Encryption     : none"
-echo -e "Network        : ws"
-echo -e "Path           : /vless"
-echo -e "Path           : vless-grpc"
-echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-echo -e "Link TLS       : ${vlesslink1}"
-echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-echo -e "Link none TLS  : ${vlesslink2}"
-echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-echo -e "Link GRPC      : ${vlesslink3}"
-echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-echo -e "Expired On     : $exp"
-echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m" | tee -a /etc/log-create-user.log
+echo -e "\e[1;36m    .::::.  Trial Xray/Vless  .::::.  \033[0m" | tee -a /etc/log-create-user.log
+echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m" | tee -a /etc/log-create-user.log
+echo -e "Remarks     : ${user}" | tee -a /etc/log-create-user.log
+echo -e "Domain      : ${domain}" | tee -a /etc/log-create-user.log
+echo -e "port TLS    : $tls" | tee -a /etc/log-create-user.log
+echo -e "port nn TLS : $none" | tee -a /etc/log-create-user.log
+echo -e "User id     : ${uuid}" | tee -a /etc/log-create-user.log
+echo -e "Encryption  : none" | tee -a /etc/log-create-user.log
+echo -e "Network     : ws" | tee -a /etc/log-create-user.log
+echo -e "Path        : /vless" | tee -a /etc/log-create-user.log
+echo -e "Path        : vless-grpc" | tee -a /etc/log-create-user.log
+echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m" | tee -a /etc/log-create-user.log
+echo -e "Link TLS      : ${vlesslink1}" | tee -a /etc/log-create-user.log
+echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m" | tee -a /etc/log-create-user.log
+echo -e "Link none TLS : ${vlesslink2}" | tee -a /etc/log-create-user.log
+echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m" | tee -a /etc/log-create-user.log
+echo -e "Link GRPC     : ${vlesslink3}" | tee -a /etc/log-create-user.log
+echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m" | tee -a /etc/log-create-user.log
+echo -e "Expired On   : $exp" | tee -a /etc/log-create-user.log
+echo -e "@HRstores" | tee -a /etc/log-create-user.log
 echo ""
 
 read -n 1 -s -r -p "Press any key to back on menu"
