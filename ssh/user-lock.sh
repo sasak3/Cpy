ace8f9cfec057543ab893ad2d2813b05cee9e371
heruahmad1 <138838307+heruahmad1@users.noreply.github.com>
2023-12-02 15:04:20 +0800

    Update user-lock.sh

--- ssh/user-lock.sh
+++ ssh/user-lock.sh
@@ -67,7 +67,11 @@ fi
 echo -e "\e[32mloading...\e[0m"
 clear
 echo " "
-read -p "Input Username you want to lock: " username
+echo -e "\e[33m ┌───────────────────────────────────┐\033[0m"
+echo -e "\e[33m │\e[1;36m .::::.  LOCK SSH OVPN  .::::.  \033[0m"
+echo -e "\e[33m └───────────────────────────────────┘\033[0m"
+echo -e ""
+read -p "  Input Username you want to lock: " username
 egrep "^$username" /etc/passwd >/dev/null
 if [ $? -eq 0 ]; then
 # proses mengganti passwordnya
@@ -76,10 +80,10 @@ clear
   echo " "
   echo " "
   echo " "
-  echo "-----------------------------------------------"
-  echo -e "Username ${blue}$username${NC} successfully ${red}LOCKED!${NC}."
-  echo -e "Access Login to username ${blue}$username${NC} has been locked."
-  echo "-----------------------------------------------"
+  echo " ─────────────────────────────────────"
+  echo -e " Username ${blue}$username${NC} successfully ${red}LOCKED!${NC}."
+  echo -e " Access Login to username ${blue}$username${NC} has been locked."
+  echo " ─────────────────────────────────────"
 else
 echo "Username not found on your server."
     exit 1
