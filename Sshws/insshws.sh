6f9e5c9da08f0ad83ab94f5fff72a7a3ab986b40
sasak3 <152268056+sasak3@users.noreply.github.com>
2023-12-04 01:43:17 +0800

    Update insshws.sh

--- Sshws/insshws.sh
+++ Sshws/insshws.sh
@@ -4,7 +4,7 @@
 cd
 
 #Install Script Websocket-SSH Python
-#wget -O /usr/local/bin/ws-openssh https://raw.githubusercontent.com/artanodrop/v4/main/sshws/openssh-socket.py
+#wget -O /usr/local/bin/ws-openssh https://raw.githubusercontent.com/sasak3/v4/main/sshws/openssh-socket.py
 wget -O /usr/local/bin/ws-dropbear https://raw.githubusercontent.com/sasak3/v4/main/Sshws/dropbear-ws.py
 wget -O /usr/local/bin/ws-stunnel https://raw.githubusercontent.com/sasak3/v4/main/Sshws/ws-stunnel
 #wget -O /usr/local/bin/ws-ovpn https://raw.githubusercontent.com/${GitUser}/test1/${namafolder}/main/ws-ovpn && chmod +x /usr/local/bin/ws-ovpn
