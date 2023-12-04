dda22163943706b139e44ca70da6f6d9751b0709
sasak3 <152268056+sasak3@users.noreply.github.com>
2023-12-04 22:03:05 +0800

    Update ins-xray.sh

--- xray/ins-xray.sh
+++ xray/ins-xray.sh
@@ -602,35 +602,7 @@ systemctl stop trojan-go
 systemctl start trojan-go
 systemctl enable trojan-go
 systemctl restart trojan-go
-#IFORMASI
-fun_bar() {
-    CMD[0]="$1"
-    CMD[1]="$2"
-    (
-        [[ -e $HOME/fim ]] && rm $HOME/fim
-        ${CMD[0]} -y >/dev/null 2>&1
-        ${CMD[1]} -y >/dev/null 2>&1
-        touch $HOME/fim
-    ) >/dev/null 2>&1 &
-    tput civis
-    echo -ne "  \033[0;33mPlease Wait Loading \033[1;37m- \033[0;33m["
-    while true; do
-        for ((i = 0; i < 18; i++)); do
-            echo -ne "\033[0;32m▮"
-            sleep 0.1s
-        done
-        [[ -e $HOME/fim ]] && rm $HOME/fim && break
-        echo -e "\033[0;33m]"
-        sleep 1s
-        tput cuu1
-        tput dl1
-        echo -ne "  \033[0;33mPlease Wait Loading \033[1;37m- \033[0;33m["
-    done
-    echo -e "\033[0;33m]\033[1;37m -\033[1;32m OK !\033[1;37m"
-    tput cnorm
-}
-res1() {
-cd /usr/bin/
+
 # vmess
 wget -O add-ws "https://raw.githubusercontent.com/sasak3/v4/main/xray/add-ws.sh" && chmod +x add-ws
 wget -O trialvmess "https://raw.githubusercontent.com/sasak3/v4/main/xray/trialvmess.sh" && chmod +x trialvmess
@@ -661,10 +633,6 @@ wget -O cektrgo "https://raw.githubusercontent.com/sasak3/v4/main/xray/cektrgo.s
 
 # bot panell
 wget -O bot "https://raw.githubusercontent.com/sasak3/v4/main/bot.sh" && chmod +x /usr/bin/bot
-}
-netfilter-persistent
-clear
-fun_bar 'res1'
 
 sleep 1
 yellow() { echo -e "\\033[33;1m${*}\\033[0m"; }
