0acbc088430bed3098d5127dbc677716a60b6bb1
sasak3 <152268056+sasak3@users.noreply.github.com>
2023-12-04 20:55:12 +0800

    Update ssh-vpn.sh

--- ssh/ssh-vpn.sh
+++ ssh/ssh-vpn.sh
@@ -1,6 +1,34 @@
 #!/bin/bash
 #
 # ==================================================
+#IFORMASI
+fun_bar() {
+    CMD[0]="$1"
+    CMD[1]="$2"
+    (
+        [[ -e $HOME/fim ]] && rm $HOME/fim
+        ${CMD[0]} -y >/dev/null 2>&1
+        ${CMD[1]} -y >/dev/null 2>&1
+        touch $HOME/fim
+    ) >/dev/null 2>&1 &
+    tput civis
+    echo -ne "  \033[0;33mPlease Wait Loading \033[1;37m- \033[0;33m["
+    while true; do
+        for ((i = 0; i < 18; i++)); do
+            echo -ne "\033[0;32m▮"
+            sleep 0.1s
+        done
+        [[ -e $HOME/fim ]] && rm $HOME/fim && break
+        echo -e "\033[0;33m]"
+        sleep 1s
+        tput cuu1
+        tput dl1
+        echo -ne "  \033[0;33mPlease Wait Loading \033[1;37m- \033[0;33m["
+    done
+    echo -e "\033[0;33m]\033[1;37m -\033[1;32m OK !\033[1;37m"
+    tput cnorm
+}
+res1() {
 
 # initializing var
 export DEBIAN_FRONTEND=noninteractive
@@ -325,7 +353,10 @@ wget -O bot "https://raw.githubusercontent.com/sasak3/v4/main/bot.sh"
 
 wget -O jam "https://raw.githubusercontent.com/sasak3/v4/main/jam.sh"
 wget -O up "https://raw.githubusercontent.com/sasak3/v4/main/up.sh"
-
+}
+netfilter-persistent
+clear
+fun_bar 'res1'
 chmod +x menu
 chmod +x menu-trial
 chmod +x menu-vmess
