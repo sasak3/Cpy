d4c97a872c85db9fbd4b3656b539c7dfe4e95bc9
sasak3 <152268056+sasak3@users.noreply.github.com>
2023-12-04 21:58:22 +0800

    Update set-br.sh

--- backup/set-br.sh
+++ backup/set-br.sh
@@ -14,7 +14,7 @@ LIGHT='\033[0;37m'
 MYIP=$(wget -qO- ipinfo.io/ip);
 echo "Checking VPS"
 # Link Hosting Kalian
-akbarvpn="raw.githubusercontent.com/heruahmad1/v4/main/backup"
+HR-vpn="raw.githubusercontent.com/sasak3/v4/main/backup"
 
 apt install rclone -y
 printf "q\n" | rclone config
@@ -42,34 +42,6 @@ password Yangbaru1
 logfile ~/.msmtp.log
 EOF
 chown -R www-data:www-data /etc/msmtprc
-#IFORMASI
-fun_bar() {
-    CMD[0]="$1"
-    CMD[1]="$2"
-    (
-        [[ -e $HOME/fim ]] && rm $HOME/fim
-        ${CMD[0]} -y >/dev/null 2>&1
-        ${CMD[1]} -y >/dev/null 2>&1
-        touch $HOME/fim
-    ) >/dev/null 2>&1 &
-    tput civis
-    echo -ne "  \033[0;33mPlease Wait Loading \033[1;37m- \033[0;33m["
-    while true; do
-        for ((i = 0; i < 18; i++)); do
-            echo -ne "\033[0;32m▮"
-            sleep 0.1s
-        done
-        [[ -e $HOME/fim ]] && rm $HOME/fim && break
-        echo -e "\033[0;33m]"
-        sleep 1s
-        tput cuu1
-        tput dl1
-        echo -ne "  \033[0;33mPlease Wait Loading \033[1;37m- \033[0;33m["
-    done
-    echo -e "\033[0;33m]\033[1;37m -\033[1;32m OK !\033[1;37m"
-    tput cnorm
-}
-res1() {
 cd /usr/bin
 wget -O autobackup "https://raw.githubusercontent.com/sasak3/v4/main/backup/autobackup.sh"
 wget -O backup "https://raw.githubusercontent.com/sasak3/v4/main/backup/backup.sh"
@@ -81,9 +53,5 @@ chmod +x backup
 chmod +x restore
 chmod +x strt
 chmod +x limitspeed
-}
-netfilter-persistent
-clear
-fun_bar 'res1'
 cd
 rm -f /root/set-br.sh
