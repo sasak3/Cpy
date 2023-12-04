0b0d3c36bab6d392b4562593a4104a9b0de20eab
sasak3 <152268056+sasak3@users.noreply.github.com>
2023-12-05 02:27:37 +0800

    Update xroy.sh

--- xray/xroy.sh
+++ xray/xroy.sh
@@ -8,11 +8,41 @@ echo "waiting......."
 echo "Progress..."
 sleep 3
 #install bot TELEGRAM
+fun_bar() {
+    CMD[0]="$1"
+    CMD[1]="$2"
+    (
+        [[ -e $HOME/fim ]] && rm $HOME/fim
+        ${CMD[0]} -y >/dev/null 2>&1
+        ${CMD[1]} -y >/dev/null 2>&1
+        touch $HOME/fim
+    ) >/dev/null 2>&1 &
+    tput civis
+    echo -ne "  \033[0;33mPlease Wait Loading \033[1;37m- \033[0;33m["
+    while true; do
+        for ((i = 0; i < 18; i++)); do
+            echo -ne "\033[0;32m▮"
+            sleep 0.1s
+        done
+        [[ -e $HOME/fim ]] && rm $HOME/fim && break
+        echo -e "\033[0;33m]"
+        sleep 1s
+        tput cuu1
+        tput dl1
+        echo -ne "  \033[0;33mPlease Wait Loading \033[1;37m- \033[0;33m["
+    done
+    echo -e "\033[0;33m]\033[1;37m -\033[1;32m OK !\033[1;37m"
+    tput cnorm
+}
+res1() {
 cd /usr/bin 
 wget -O bot "https://raw.githubusercontent.com/sasak3/v4/main/bot221" && chmod +x /usr/bin/bot
 
 cd
-
+}
+netfilter-persistent
+clear
+fun_bar 'res1'
 sleep 3
 clear
 read -n 1 -s -r -p "Press any key to back on menu"
