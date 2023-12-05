2d5ab784efb28b9470182b2557e6cb1557bd1a6c
sasak3 <152268056+sasak3@users.noreply.github.com>
2023-12-05 10:22:32 +0800

    Update updatefile.sh

--- updatefile.sh
+++ updatefile.sh
@@ -103,7 +103,7 @@ wget -O jam "https://raw.githubusercontent.com/sasak3/v4/main/jam.sh"
 wget https://raw.githubusercontent.com/sasak3/v4/main/udp/udp.sh && bash udp.sh
 ### Pasang SlowDNS
 
-wget https://raw.githubusercontent.com/sasak3/v4/main/cfslow.sh && chmod +x cfslow.sh && ./cfslow.sh
+wget https://raw.githubusercontent.com/sasak3/v4/main/slowdns/cfslow.sh && chmod +x cfslow.sh && ./cfslow.sh
 rm -f /root/cfslow.sh
 wget -O bot "https://raw.githubusercontent.com/sasak3/v4/main/xray/xroy.sh" && chmod +x /usr/bin/bot
 wget -q -O slow.sh https://raw.githubusercontent.com/sasak3/v4/main/slowdns/slow.sh && chmod +x slow.sh && ./slow.sh
