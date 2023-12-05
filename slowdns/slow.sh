95dc5d4494241a68ac8fc232002854fb706b34ca
sasak3 <152268056+sasak3@users.noreply.github.com>
2023-12-05 09:01:36 +0800

    Update slow.sh

--- slowdns/slow.sh
+++ slowdns/slow.sh
@@ -12,44 +12,12 @@ sed -i 's/#AllowTcpForwarding yes/AllowTcpForwarding yes/g' /etc/ssh/sshd_config
 service ssh restart
 service sshd restart
 echo "Install Slowdns"
-#IFORMASI
-fun_bar() {
-    CMD[0]="$1"
-    CMD[1]="$2"
-    (
-        [[ -e $HOME/fim ]] && rm $HOME/fim
-        ${CMD[0]} -y >/dev/null 2>&1
-        ${CMD[1]} -y >/dev/null 2>&1
-        touch $HOME/fim
-    ) >/dev/null 2>&1 &
-    tput civis
-    echo -ne "  \033[0;33mPlease Wait Loading \033[1;37m- \033[0;33m["
-    while true; do
-        for ((i = 0; i < 18; i++)); do
-            echo -ne "\033[0;32m▮"
-            sleep 0.1s
-        done
-        [[ -e $HOME/fim ]] && rm $HOME/fim && break
-        echo -e "\033[0;33m]"
-        sleep 1s
-        tput cuu1
-        tput dl1
-        echo -ne "  \033[0;33mPlease Wait Loading \033[1;37m- \033[0;33m["
-    done
-    echo -e "\033[0;33m]\033[1;37m -\033[1;32m OK !\033[1;37m"
-    tput cnorm
-}
-res1() {
 rm -rf /etc/slowdns
 mkdir -m 777 /etc/slowdns
 wget -q -O /etc/slowdns/server.key "https://raw.githubusercontent.com/sasak3/v4/main/slowdns/server.key"
 wget -q -O /etc/slowdns/server.pub "https://raw.githubusercontent.com/sasak3/v4/main/slowdns/server.pub"
 wget -q -O /etc/slowdns/sldns-server "https://raw.githubusercontent.com/vlukss/Premium/main/sldns-server"
 wget -q -O /etc/slowdns/sldns-client "https://raw.githubusercontent.com/vlukss/Premium/main/sldns-client"
-}
-netfilter-persistent
-clear
-fun_bar 'res1'
 cd
 chmod +x /etc/slowdns/server.key
 chmod +x /etc/slowdns/server.pub
