df2ee46e6d14f526b6f83775902d02b430117189
sasak3 <152268056+sasak3@users.noreply.github.com>
2023-12-06 13:34:59 +0800

    Update add-host.sh

--- ssh/add-host.sh
+++ ssh/add-host.sh
@@ -20,6 +20,6 @@ echo "IP=$host" > /var/lib/SIJA/ipvps.conf
 echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
 echo "Dont forget to renew cert"
 echo ""
-read -n 1 -s -r -p "Press any key to back on menu"
-menu
+read -n 1 -s -r -p "tekan enter to renew cert"
+certv2ray
 fi
